d4a8bd8f9e77c7c0b486960dc7908b13756be4fe
Muhammad Zain Raza <zainraza20000@gmail.com>
2024-05-08 21:01:57 +0500

    ui changes

---
+++ src/components/modal/FormModal.tsx
@@ -0,0 +1,157 @@
+import Image from 'next/image';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import InputField from 'components/fields/InputField';
+
+interface FormModal {
+  open: boolean;
+  setOpen: any;
+  cancelButtonRef: any;
+}
+
+const FormModal: React.FC<FormModal> = ({ open, setOpen, cancelButtonRef }) => {
+  const [formData, setFormData] = useState({
+    institution: '',
+    location: '',
+    specialty: '',
+    yearOfEducation: '',
+  });
+  const [formErrors, setFormErrors] = useState({
+    institution: '',
+    location: '',
+    specialty: '',
+    yearOfEducation: '',
+  });
+
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const valid = await validateForm();
+    if (valid) {
+      console.log(formData);
+      setOpen(false);
+    }
+  };
+
+  const validateForm = async () => {
+    let tempErrors = { ...formErrors };
+    let valid = true;
+    Object.keys(formData).map((key) => {
+      if (!formData[key]) {
+        tempErrors = { ...formErrors, [key]: 'Field is required' };
+        valid = false;
+      }
+    });
+
+    setFormErrors(tempErrors);
+    return valid;
+  };
+  useEffect(() => {
+    console.log(formErrors);
+  }, [formErrors]);
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-[5000000]"
+        initialFocus={cancelButtonRef}
+        onClose={setOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel
+                className={`relative w-[500px] transform  overflow-hidden rounded-lg bg-white p-8 text-left shadow-xl transition-all`}
+              >
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-5">
+                    <InputField
+                      id="institution"
+                      type="text"
+                      name="institution"
+                      placeholder="Enter your institution"
+                      label="Institution"
+                      onChange={handleInputChange}
+                      state={formErrors.institution ? 'error' : ''}
+                    />
+                  </div>
+                  <div className="mb-5">
+                    <InputField
+                      id="location"
+                      label="Location"
+                      type="text"
+                      name="location"
+                      placeholder="Enter your location"
+                      onChange={handleInputChange}
+                      state={formErrors.location ? 'error' : ''}
+                    />
+                  </div>
+                  <div className="mb-5">
+                    <InputField
+                      id="specialty"
+                      label="Specialty"
+                      type="text"
+                      name="specialty"
+                      placeholder="Enter your specialty"
+                      onChange={handleInputChange}
+                      state={formErrors.specialty ? 'error' : ''}
+                    />
+                  </div>
+                  <div className="mb-5">
+                    <InputField
+                      id="yearOfEducation"
+                      label="YearOfEducation"
+                      type="text"
+                      name="yearOfEducation"
+                      placeholder="Enter your year of education"
+                      onChange={handleInputChange}
+                      state={formErrors.yearOfEducation ? 'error' : ''}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <button
+                      type="submit"
+                      className="linear w-full rounded-xl bg-bluePrimary py-3 text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+export default FormModal;
